e6f77adbac79a67c9c5210e454c1c126a7a5ee68
Angkush <angkysahu2502@gmail.com>
2023-09-13 19:53:56 +0530

    toggled httpOnly option off

--- src/apiModules/auth/auth.service.ts
+++ src/apiModules/auth/auth.service.ts
@@ -14,7 +14,7 @@ export class AuthService {
       return {
          expires: new Date(Date.now() + Number(process.env.COOKIE_EXPIRE) * 24 * 60 * 60 * 1000),
          secure: process.env.NODE_ENV === "production",
-         httpOnly: true,
+         // httpOnly: true,
          sameSite: false,
          domain: process.env["BROWSER_URL"],
       };
